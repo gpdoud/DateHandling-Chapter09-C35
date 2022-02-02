84d918ec8a8ca4d987626eb07ebdc2f081410c5f
Greg Doud <gdoud@gmail.com>
2022-02-02 15:43:59 -0500

    Add project files.

---
+++ DateHandling/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace DateHandling
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnCalculateAge = new System.Windows.Forms.Button();
+            this.btnCalculateDueDays = new System.Windows.Forms.Button();
+            this.txtBirthDate = new System.Windows.Forms.TextBox();
+            this.txtFutureDate = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(264, 90);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(178, 23);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnCalculateAge
+            // 
+            this.btnCalculateAge.Location = new System.Drawing.Point(264, 50);
+            this.btnCalculateAge.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnCalculateAge.Name = "btnCalculateAge";
+            this.btnCalculateAge.Size = new System.Drawing.Size(178, 23);
+            this.btnCalculateAge.TabIndex = 10;
+            this.btnCalculateAge.Text = "Calculate &Age";
+            this.btnCalculateAge.Click += new System.EventHandler(this.btnCalculateAge_Click);
+            // 
+            // btnCalculateDueDays
+            // 
+            this.btnCalculateDueDays.Location = new System.Drawing.Point(264, 10);
+            this.btnCalculateDueDays.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnCalculateDueDays.Name = "btnCalculateDueDays";
+            this.btnCalculateDueDays.Size = new System.Drawing.Size(178, 24);
+            this.btnCalculateDueDays.TabIndex = 8;
+            this.btnCalculateDueDays.Text = "Calculate &Due Days";
+            this.btnCalculateDueDays.Click += new System.EventHandler(this.btnCalculateDueDays_Click);
+            // 
+            // txtBirthDate
+            // 
+            this.txtBirthDate.Location = new System.Drawing.Point(147, 50);
+            this.txtBirthDate.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtBirthDate.Name = "txtBirthDate";
+            this.txtBirthDate.Size = new System.Drawing.Size(96, 23);
+            this.txtBirthDate.TabIndex = 9;
+            this.txtBirthDate.Text = "10/24/52";
+            // 
+            // txtFutureDate
+            // 
+            this.txtFutureDate.Location = new System.Drawing.Point(147, 10);
+            this.txtFutureDate.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtFutureDate.Name = "txtFutureDate";
+            this.txtFutureDate.Size = new System.Drawing.Size(96, 23);
+            this.txtFutureDate.TabIndex = 5;
+            this.txtFutureDate.Text = "12/31/2020";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(14, 50);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 23);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Enter a birth date:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(14, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 23);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Enter a future date:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCalculateDueDays;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(471, 127);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculateAge);
+            this.Controls.Add(this.btnCalculateDueDays);
+            this.Controls.Add(this.txtBirthDate);
+            this.Controls.Add(this.txtFutureDate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Date Handling";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnCalculateAge;
+        private System.Windows.Forms.Button btnCalculateDueDays;
+        private System.Windows.Forms.TextBox txtBirthDate;
+        private System.Windows.Forms.TextBox txtFutureDate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DateHandling/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateHandling
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculateDueDays_Click(object sender, System.EventArgs e)
+        {
+            // TODO: Add code to calculate the days until due date
+        }
+
+        private void btnCalculateAge_Click(object sender, System.EventArgs e)
+        {
+            // TODO: Add code to calculate the age
+        }
+
+        private void btnExit_Click(object sender, System.EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
