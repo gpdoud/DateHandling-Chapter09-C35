5ae6ff7625de529a2a0ea7765562527c7f1ce9d9
Greg Doud <gdoud@gmail.com>
2022-02-02 17:25:20 -0500

    Calculate age in years

--- DateHandling/Form1.cs
+++ DateHandling/Form1.cs
@@ -21,18 +21,30 @@ namespace DateHandling
         private void btnCalculateDueDays_Click(object sender, System.EventArgs e)
         {
             // TODO: Add code to calculate the days until due date
-            DateTime futureDate = DateTime.Parse(txtFutureDate.Text, 
-                                                CultureInfo.CurrentCulture,
-                                                DateTimeStyles.AssumeLocal);
+            DateTime currentDate = DateTime.Today;
             DateTime birthDate = DateTime.Parse(txtBirthDate.Text,
                                                 CultureInfo.CurrentCulture,
                                                 DateTimeStyles.AssumeLocal);
-            int days = futureDate.Subtract(birthDate).Days;
+            int days = currentDate.Subtract(birthDate).Days;
         }
 
         private void btnCalculateAge_Click(object sender, System.EventArgs e)
         {
             // TODO: Add code to calculate the age
+            DateTime birthDate = DateTime.Parse(txtBirthDate.Text,
+                                                CultureInfo.CurrentCulture,
+                                                DateTimeStyles.AssumeLocal);
+            DateTime currentDate = DateTime.Today;
+            long ticks = currentDate.Ticks - birthDate.Ticks;
+            int years = new DateTime(ticks).Year;
+            if(currentDate.DayOfYear < birthDate.DayOfYear) {
+                years--;
+            }
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Current Date:\t {currentDate.ToString("d")}\n\n");
+            sb.Append($"Birth Date  :\t {birthDate.ToString("d")}\n\n");
+            sb.Append($"Age         :\t\t {years}");
+            MessageBox.Show(sb.ToString(), "Age Calculation");
         }
 
         private void btnExit_Click(object sender, System.EventArgs e)
