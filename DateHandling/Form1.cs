fd98f6c3f3992319f00211917e7e0235f8cebf20
Greg Doud <gdoud@gmail.com>
2022-02-02 16:01:34 -0500

    Allow multiple datetime formats

--- DateHandling/Form1.cs
+++ DateHandling/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,8 +21,12 @@ namespace DateHandling
         private void btnCalculateDueDays_Click(object sender, System.EventArgs e)
         {
             // TODO: Add code to calculate the days until due date
-            DateTime futureDate = Convert.ToDateTime(txtFutureDate.Text);
-            DateTime birthDate = Convert.ToDateTime(txtBirthDate.Text);
+            DateTime futureDate = DateTime.Parse(txtFutureDate.Text, 
+                                                CultureInfo.CurrentCulture,
+                                                DateTimeStyles.AssumeLocal);
+            DateTime birthDate = DateTime.Parse(txtBirthDate.Text,
+                                                CultureInfo.CurrentCulture,
+                                                DateTimeStyles.AssumeLocal);
             int days = futureDate.Subtract(birthDate).Days;
         }
 
