281eb9556270a8742ae7600ca57b779b8937445f
Greg Doud <gpdoud@gmail.com>
2022-02-03 13:52:30 -0500

    Finished

--- DateHandling/Form1.cs
+++ DateHandling/Form1.cs
@@ -22,14 +22,15 @@ namespace DateHandling
             // TODO: Add code to calculate the days until due date
             DateTime currentDate = DateTime.Today;
             DateTime futureDate = Convert.ToDateTime(txtFutureDate.Text);
-            int days = futureDate.Subtract(currentDate).Days;
+            TimeSpan timespan = futureDate.Subtract(currentDate);
+            int days = timespan.Days;
 
             string message = string.Empty;
             message += $"Current date:\t{currentDate:d}\n\n";
             message += $"Future date: \t{futureDate:d}\n\n";
             message += $"Days until due :\t{days}\n\n";
 
-            MessageBox.Show(message, "Due Days Calculateion");
+            MessageBox.Show(message, "Due Days Calculation");
         }
 
         private void btnCalculateAge_Click(object sender, System.EventArgs e)
